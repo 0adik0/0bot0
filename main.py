90cc46b005b5d88482d9605c0852db212c088e87
0adik0 <FoxLink48@mail.ru>
2023-04-21 23:20:57 +0200

    update

--- main.py
+++ main.py
@@ -1,85 +1,95 @@
-import asyncio
-import requests
+import os
+import logging
+import uuid
 from aiogram import Bot, Dispatcher, types
-from aiogram.utils import executor
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
+from aiogram.types import ParseMode, InlineKeyboardMarkup, InlineKeyboardButton
+import requests
 
-import markup
-from database.database import Database
-TOKEN = '6012383203:AAEtTX-jxszwteMinaKBE4vzOI15luSt2lM'
+API_TOKEN = os.environ.get('API_TOKEN', '1619406127:AAFcy3-mwwwt13d_KEqaqfsvKMdLeaTXbaE')
+WEB_APP_URL = os.environ.get('WEB_APP_URL', 'https://privatizerbot.herokuapp.com')
 
-bot = Bot(token=TOKEN)
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=API_TOKEN)
+
 dp = Dispatcher(bot)
-db = Database("database/database.db")
-# Состояния ссылок
-# states = {"Найдено": 1, "Не найдено": 0}
+dp.middleware.setup(LoggingMiddleware())
 
+state = {}
 
-# Обработчик команды /start
-@dp.message_handler(commands=['start'])
-async def start(message: types.Message):
-    await message.reply("Отправь мне ссылку на приложение в Google Play Store для проверки.", reply_markup=markup.main_markup)
+async def on_start(message: types.Message):
+    markup = InlineKeyboardMarkup(row_width=1)
+    markup.add(
+        InlineKeyboardButton("Создать Privacy Policy", callback_data="create_privacy_policy"),
+        InlineKeyboardButton("Создать Terms of Use", callback_data="create_terms_of_use"),
+    )
+    await message.reply("Продолжите фразу: «Сегодня я хочу...»", reply_markup=markup)
 
+async def on_callback_query(call: types.CallbackQuery):
+    action = call.data
+    chat_id = call.message.chat.id
 
-# Обработчик текстовых сообщений (ссылок)
-@dp.message_handler()
-async def echo(message: types.Message):
-    # Показ всех ссылок пользователя
-    if message.text == "Мои ссылки":
+    if action == "create_privacy_policy":
+        state[chat_id] = {"step": 1, "document_type": "privacy_policy"}
+        await call.message.reply("Введите имя разработчика:")
+    elif action == "create_terms_of_use":
+        state[chat_id] = {"step": 1, "document_type": "terms_of_use"}
+        await call.message.reply("Введите имя разработчика:")
 
-        links = db.get_links_by_user(message.chat.id)
-        if len(links) > 0:
-            msg = ""
-            for link in links:
+async def on_message(message: types.Message):
+    chat_id = message.chat.id
 
-                if link[3] == 1:
-                    msg += f"{link[2]} - в сторе\n\n"
-                else:
-                    msg += f"{link[2]} - не в сторе\n\n"
-            await bot.send_message(message.chat.id, msg)
-        else:
-            await bot.send_message(message.chat.id, "У вас нет добавленных ссылок!")
-    # Удаление ссылок
+    if chat_id not in state:
+        return
 
-    if message.text == "Удалить все ссылки":
-        db.delete_all_links(message.chat.id)
-        await bot.send_message(message.chat.id, "Удалил все ссылки!")
-    # Проверяем, что сообщение содержит ссылку
-    elif message.text.startswith('http'):
-        # Получаем список ссылок для текущего пользователя или создаем новый, если его еще нет
-        user_id = message.chat.id
-        # user_link_list = user_links.get(user_id, [])
-        # Добавляем новую ссылку в список
-        db.add_link(user_id, message.text)
-        # user_link_list.append(message.text)
-        # user_links[user_id] = user_link_list
-        await message.reply("Ссылка сохранена")
-    else:
-        await message.reply("Отправь мне ссылку на приложение в Google Play Store для проверки.")
+    step = state[chat_id]["step"]
 
+    if step == 1:
+        state[chat_id]["developer_name"] = message.text
+        state[chat_id]["step"] = 2
+        await message.reply("Введите название приложения:")
+    elif step == 2:
+        state[chat_id]["app_name"] = message.text
+        state[chat_id]["step"] = 3
+        await message.reply("Введите ваш email:")
+    elif step == 3:
+        state[chat_id]["email"] = message.text
+        document_type = state[chat_id]["document_type"]
 
-async def check_links():
-    while True:
-        links = db.get_links()
+        response = requests.post(
+            f"{WEB_APP_URL}/api/create_document",
+            json={
+                "document_type": document_type,
+                "app_name": state[chat_id]["app_name"],
+                "developer_name": state[chat_id]["developer_name"],
+                "email": state[chat_id]["email"],
+            },
+        )
+        try:
+            response.raise_for_status()
+            response_json = response.json()
+        except requests.exceptions.HTTPError as e:
+            print(f"Ошибка: {e}")
+            await message.reply("Произошла ошибка при обработке запроса. Пожалуйста, попробуйте еще раз.")
+            return
+        except ValueError as e:
+            print(f"Ошибка: {e}")
+            await message.reply("Произошла ошибка при обработке ответа сервера. Пожалуйста, попробуйте еще раз.")
+            return
 
-        for user_id, link, state in links:
-            response = requests.get(link)
-            if response.status_code == 200 and "Страница не найдена" not in response.text:
-                # Извлекаем название приложения из ссылки
-                if state == 0:
-                    db.update_state_link(link, 1)
-                    app_name = link.split("/details?id=")[-1]
-                    await bot.send_message(user_id, f"{app_name} прошла модерку")
-            else:
-                if state == 1:
-                    db.update_state_link(link, 0)
-                    app_name = link.split("/details?id=")[-1]
-                    await bot.send_message(user_id, f"Приложение {app_name} нет в сторе")
-
-        await asyncio.sleep(10)
-
+        document_url = f"{WEB_APP_URL}{response_json['url']}"
+        await message.reply(f"Ваша ссылка на {document_type.replace('_', ' ').capitalize()}: {document_url}")
+        del state[chat_id]
 
 if __name__ == '__main__':
-    # Запускаем асинхронную функцию проверки ссылок
-    loop = asyncio.get_event_loop()
-    loop.create_task(check_links())
-    executor.start_polling(dp, loop=loop, skip_updates=True)
+    from aiogram import executor
+    from aiogram.contrib.middlewares.logging import LoggingMiddleware
+
+    dp.middleware.setup(LoggingMiddleware())
+
+    dp.register_message_handler(on_start, commands=['start'])
+    dp.register_message_handler(on_message)
+    dp.register_callback_query_handler(on_callback_query)
+
+    executor.start_polling(dp, skip_updates=True)
